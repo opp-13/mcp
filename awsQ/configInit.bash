b82920f2e966e4410efab6c0959e0001c2fa5938
Sungho, Kim <john_sungho@naver.com>
2025-09-05 16:33:54 +0900

    fix: 시작 스크립트 수정

---
+++ awsQ/configInit.bash
@@ -0,0 +1,5 @@
+#! /bin.bash
+
+## ln -s ./mcp.json ~/.aws/amazonq/mcp.json
+ln -s ./rules/rules.md ~/.aws/amazonq/rules/rules.md
+ln -s ./cli-agents/mcp-dev.json ~/.aws/amazonq/cli-agents/mcp-dev.json
